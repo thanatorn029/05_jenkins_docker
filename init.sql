40f5b8ef135f4118863cdbab86e00137fbcb1f21
thanatorn029 <thanathorn.k66@rsu.ac.th>
2025-12-01 09:47:14 +0700

    init.sql

--- init.sql
+++ init.sql
@@ -11,12 +11,12 @@ INSERT INTO `attraction` (`id`, `name`, `detail`, `coverimage`, `latitude`, `lon
 (1, 'Silverstone Circuit', 'The home of British motorsport, hosting Formula 1 British Grand Prix.', '/Silverstone Circuit.webp', 52.0786, -1.0169),
 (2, 'Monaco Grand Prix Circuit', 'Famous street circuit in Monte Carlo, known for its tight corners and luxury backdrop.', 'Monaco Grand Prix Circuit.webp', 43.7347, 7.4206),
 (3, 'Suzuka Circuit', 'Iconic Japanese track with figure-eight layout, hosting F1 Japanese Grand Prix.', 'Suzuka Circuit.avif', 34.8431, 136.5410),
-(4, 'Circuit de Spa-Francorchamps', 'Legendary Belgian track, known for Eau Rouge corner and unpredictable weather.', 'Circuit of the Americas.avif', 50.4372, 5.9714),
+(4, 'Circuit de Spa-Francorchamps', 'Legendary Belgian track, known for Eau Rouge corner and unpredictable weather.', 'Circuit de Spa-Francorchamps.jpg', 50.4372, 5.9714),
 (5, 'Autodromo Nazionale Monza', 'Historic Italian circuit near Milan, famous for high-speed straights.', 'Autodromo Nazionale Monza.avif', 45.6156, 9.2811),
 (6, 'Laguna Seca', 'Famous US track in California, known for the challenging Corkscrew turn.', 'Laguna Seca.avif', 36.5841, -121.7480),
 (7, 'Circuit of the Americas', 'Austin, Texas track hosting F1 US Grand Prix, known for its elevation changes.', 'Circuit of the Americas.avif', 30.1328, -97.6411),
-(8, 'Interlagos (Autódromo José Carlos Pace)', 'Brazilian F1 circuit in São Paulo, famous for its passionate fans and unpredictable races.', 'Interlagos (Autódromo José Carlos Pace).avif', -23.7036, -46.6997),
+(8, 'Interlagos (Autodromo Jose Carlos Pace)', 'Brazilian F1 circuit in São Paulo, famous for its passionate fans and unpredictable races.', 'Interlagos (Autodromo Jose Carlos Pace).avif', -23.7036, -46.6997),
 (9, 'Yas Marina Circuit', 'Abu Dhabi F1 track with unique twilight race and marina backdrop.', 'Yas Marina Circuit.avif', 24.4672, 54.6031),
 (10, 'Red Bull Ring', 'Austrian F1 track in Spielberg, known for scenic views and fast corners.', 'Red Bull Ring.avif', 47.2197, 14.7648),
 (11, 'Hockenheimring', 'German circuit with rich F1 history, combining high-speed straights and technical corners.', 'Hockenheimring.avif', 49.3276, 8.5656),
-(12, 'Nürburgring GP-Strecke', 'Modern GP track in Germany, part of famous Nürburgring complex.', 'Nürburgring GP-Strecke.avif', 50.3356, 6.9475);
+(12, 'Nürburgring GP-Strecke', 'Modern GP track in Germany, part of famous Nürburgring complex.', 'Nrburgring GP-Strecke.avif', 50.3356, 6.9475);
